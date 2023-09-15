9d6a7103a54268743152e7be0f639c1d84d993b4
JP Marcantonio <marcantoniojp@gmail.com>
2023-09-15 00:44:28 -0400

    script to pair 2 random names from a list

---
+++ random_name_pairer_upper.py
@@ -0,0 +1,13 @@
+"""
+A Python script that has a list of student names and selects two random names and prints them.
+Example: It should print Bob => John.
+The names must be hardcoded into the 'names' list
+"""
+
+import random
+
+names = ['JP', 'Admas', 'Embelle', 'Dilu', 'Andy',  'Denika', 'Abubukar', 'Jennifer', 'Victoria', 'Maryanna']
+
+name_choice = random.sample(names, 2)
+print(f"{name_choice[0]} => {name_choice[1]}")
+
